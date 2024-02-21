af82a4a289d85e8547373fbc59e85b9a54230442
LimQB <108454419+LimQB@users.noreply.github.com>
2024-02-21 18:00:40 +0800

    Create jk.js

---
+++ jk.js
@@ -0,0 +1,20 @@
+{
+	"urls": [
+		
+		{
+			"url": "https://xhdwc.tk/0",
+			"name": "骚零接口"
+		},
+		
+		{
+			"url": "http://饭太硬.top/tv",
+			"name": "饭太硬线路"
+		},
+		
+		{
+			"url": "http://xutv.alwaysdata.net/xu1027.json",
+			"name": "网友线路"
+		}
+		
+	]
+}
