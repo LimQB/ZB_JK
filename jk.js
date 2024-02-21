6ca291e345e492c1a42bc41d10e6606cd7034416
LimQB <108454419+LimQB@users.noreply.github.com>
2024-02-21 18:09:31 +0800

    Update jk.js

--- jk.js
+++ jk.js
@@ -14,6 +14,16 @@
 		{
 			"url": "http://xutv.alwaysdata.net/xu1027.json",
 			"name": "网友线路"
+		},
+		
+		{
+			"url": "http://tv.nxog.top/m/111.php?ou=%E6%AC%A7%E6%AD%8C&mz=index2&xl=&jar=index2",
+			"name": "欧歌接口"
+		},
+		
+		{
+			"url": "http://xhww.fun:63/小米/DEMO.json",
+			"name": "小米线路"
 		}
 		
 	]
