898f163f46e27f4cc482a05733e4aeb6098b62d2
LimQB <108454419+LimQB@users.noreply.github.com>
2024-02-23 11:28:44 +0800

    Update jk.js

--- jk.js
+++ jk.js
@@ -1,30 +1,40 @@
 {
 	"urls": [
-		
+
 		{
 			"url": "https://xhdwc.tk/0",
 			"name": "骚零接口"
 		},
-		
+
 		{
 			"url": "http://饭太硬.top/tv",
 			"name": "饭太硬线路"
 		},
-		
+
 		{
 			"url": "http://xutv.alwaysdata.net/xu1027.json",
 			"name": "网友线路"
 		},
-		
+
 		{
 			"url": "http://tv.nxog.top/m/111.php?ou=%E6%AC%A7%E6%AD%8C&mz=index2&xl=&jar=index2",
 			"name": "欧歌接口"
 		},
-		
+
 		{
 			"url": "http://xhww.fun:63/小米/DEMO.json",
 			"name": "小米线路"
+		},
+
+		{
+			"url": "https://xhdwc.tk/0",
+			"name": "Ray线路"
+		},
+
+		{
+			"url": "https://tvbox.cainisi.cf",
+			"name": "菜妮丝"
 		}
-		
+
 	]
 }
