c557f104e1bb7088c1ded499f69d8002132ffed0
LimQB <108454419+LimQB@users.noreply.github.com>
2024-02-23 16:02:45 +0800

    Update jk.js

--- jk.js
+++ jk.js
@@ -34,6 +34,96 @@
 		{
 			"url": "https://tvbox.cainisi.cf",
 			"name": "菜妮丝"
+		},
+		
+		{
+			"url": "ttp://肥猫.live",
+			"name": "肥猫"
+		},
+		
+		{
+			"url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json",
+			"name": "南风"
+		},
+		
+		{
+			"url": "https://tvbox.cainisi.cf",
+			"name": "菜妮丝"
+		},
+		
+		{
+			"url": "https://100km.top/0",
+			"name": "骚零"
+		},
+		
+		{
+			"url": "http://cdn.qiaoji8.com/tvbox.json",
+			"name": "巧记"
+		},
+		
+		{
+			"url": "http://8.210.232.168/xc.json",
+			"name": "星辰"
+		},
+		
+		{
+			"url": "https://github.moeyy.xyz/https://raw.githubusercontent.com/xyq254245/xyqonlinerule/main/XYQTVBox.json",
+			"name": "江雅"
+		},
+		
+		{
+			"url": "https://mirror.ghproxy.com/raw.githubusercontent.com/gaotianliuyun/gao/master/0825.json",
+			"name": "大宇"
+		},
+		
+		{
+			"url": "https://mirror.ghproxy.com/raw.githubusercontent.com/gaotianliuyun/gao/master/js.json",
+			"name": "高天"
+		},
+		
+		{
+			"url": "https://jihulab.com/duomv/apps/-/raw/main/fast.json",
+			"name": "多多"
+		},
+		
+		{
+			"url": "http://tv.nxog.top/api.php?mz=xb&id=1&b=欧歌",
+			"name": "讴歌"
+		},
+		
+		{
+			"url": "http://jk.itvbox.cc:66/可视TV/云星日记/仓库/api.json",
+			"name": "云星"
+		},
+		
+		{
+			"url": "http://52bsj.vip:98/wuaihouse",
+			"name": "吾爱"
+		},
+		
+		{
+			"url": "https://pastebin.com/raw/5NHaxyGR",
+			"name": "道长"
+		},
+		
+		{
+			"url": "https://raw.liucn.cc/box/m.json",
+			"name": "刘备"
+		},
+		
+		{
+			"url": "https://weixine.net/ysc.json",
+			"name": "运输"
+		},
+		
+		{
+			"url": "http://52bsj.vip:98/wuai",
+			"name": "吾爱2"
+		},
+		
+		{
+			"url": "http://itvbox.cc/tvbox/云星日记/1.m3u8",
+			"name": "云星2"
 		}
 
 	]
